49e6bf01c2f10c0ed3efa351e0379ab96dc67d59
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-06-17 12:45:54 +0530

    prettier code

--- index.js
+++ index.js
@@ -1,36 +1,42 @@
-const fs = require('node:fs');
-require('dotenv').config();
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
-const { default: mongoose } = require('mongoose');
+const fs = require("node:fs");
+require("dotenv").config();
+const path = require("node:path");
+const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const { default: mongoose } = require("mongoose");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
+const foldersPath = path.join(__dirname, "commands");
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
     const commandsPath = path.join(foldersPath, folder);
-    const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+    const commandFiles = fs
+        .readdirSync(commandsPath)
+        .filter((file) => file.endsWith(".js"));
     for (const file of commandFiles) {
         const filePath = path.join(commandsPath, file);
         const command = require(filePath);
-        if ('data' in command && 'execute' in command && 'category' in command) {
+        if ("data" in command && "execute" in command && "category" in command) {
             client.commands.set(command.data.name, command);
         } else {
-            console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
+            console.log(
+                `[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`,
+            );
         }
     }
 }
 
 (async () => {
     try {
-        mongoose.set('strictQuery', false);
+        mongoose.set("strictQuery", false);
         await mongoose.connect(process.env.MONGODB_URL);
-        console.log('Connected to MongoDB');
-        const eventsPath = path.join(__dirname, 'events');
-        const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
+        console.log("Connected to MongoDB");
+        const eventsPath = path.join(__dirname, "events");
+        const eventFiles = fs
+            .readdirSync(eventsPath)
+            .filter((file) => file.endsWith(".js"));
 
         for (const file of eventFiles) {
             const filePath = path.join(eventsPath, file);
