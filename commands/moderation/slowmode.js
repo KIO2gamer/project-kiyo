29697f1c5ab59f08bb2b471ae1c78d2f634caa53
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-06-27 14:11:44 +0530

    mass updated

---
+++ commands/moderation/slowmode.js
@@ -0,0 +1,65 @@
+const {
+    SlashCommandBuilder,
+    EmbedBuilder,
+    PermissionFlagsBits,
+    ChannelType,
+  } = require('discord.js');
+  
+  module.exports = {
+    data: new SlashCommandBuilder()
+      .setName('slowmode')
+      .setDescription('Set slowmode for a channel')
+      .setDMPermission(false)
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+      .addIntegerOption(option =>
+        option
+          .setName('duration')
+          .setDescription('Slowmode duration in seconds')
+          .setRequired(true)
+      )
+      .addChannelOption(option =>
+        option
+          .setName('channel')
+          .setDescription('The channel to set slowmode for')
+          .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
+          .setRequired(false)
+      ),
+    category: 'moderation',
+    async execute(interaction) {
+      const channel = interaction.options.getChannel('channel') || interaction.channel;
+      const durationRaw = interaction.options.getInteger('duration');
+      const duration = ms(durationRaw);
+  
+      if (!channel.permissionsFor(interaction.client.user).has(PermissionFlagsBits.ManageChannels)) {
+        const noPermissionEmbed = new EmbedBuilder()
+          .setTitle('ERROR')
+          .setColor('Red')
+          .setDescription('I do not have the required permissions to set slowmode for this channel.');
+        await interaction.reply({ embeds: [noPermissionEmbed], ephemeral: true });
+        return;
+      }
+  
+      if (duration < 0 || duration > 21600) {
+        const invalidDurationEmbed = new EmbedBuilder()
+          .setTitle('ERROR')
+          .setColor('Red')
+          .setDescription('The duration must be between 0 and 21600 seconds (6 hours).');
+        await interaction.reply({ embeds: [invalidDurationEmbed], ephemeral: true });
+        return;
+      }
+  
+      await channel.setRateLimitPerUser(duration);
+  
+      const successEmbed = new EmbedBuilder()
+        .setTitle('Slowmode Set')
+        .setColor('Green')
+        .setDescription(`Slowmode for ${channel} has been set to ${duration} seconds.`)
+        .setFooter({
+          text: `Set by: ${interaction.user.username}`,
+          iconURL: `${interaction.user.avatarURL()}`,
+        });
+  
+      await interaction.reply({ embeds: [successEmbed], ephemeral: false });
+    },
+  };
+  
--- commands/moderation/unlock.js
+++ commands/moderation/unlock.js
@@ -3,55 +3,81 @@ const {
 	EmbedBuilder,
 	PermissionFlagsBits,
 	ChannelType,
-} = require('discord.js')
-
-module.exports = {
+  } = require('discord.js');
+  
+  module.exports = {
 	data: new SlashCommandBuilder()
-		.setName('unlock')
-		.setDescription('Unlock a channel')
-		.setDMPermission(false)
-		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
-		.addChannelOption((option) =>
-			option
-				.setName('channel')
-				.setDescription('The channel you want to unlock')
-				.addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
-				.setRequired(false)
-		),
-		category: 'moderation',
+	  .setName('unlock')
+	  .setDescription('Unlock a channel')
+	  .setDMPermission(false)
+	  .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+	  .addChannelOption((option) =>
+		option
+		  .setName('channel')
+		  .setDescription('The channel you want to unlock')
+		  .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
+		  .setRequired(false)
+	  ),
+	category: 'moderation',
 	async execute(interaction) {
-			const channel = interaction.options.getChannel('channel') || interaction.channel
-			await interaction.deferReply({ ephemeral: true })
-
-			const embed = new EmbedBuilder().setTitle(`${channel} has been unlocked`).setColor('Green').setFooter({
-				text: `Done by: ${interaction.user.username}`,
-				iconURL: `${interaction.user.avatarURL()}`,
-			})
-
-			const errorEmbed = new EmbedBuilder().setTitle(`ERROR`).setColor('Green').setDescription(`${channel} is already unlocked`)
-
-			
-			if (!channel.permissionOverwrites.cache.get(interaction.guild.id).deny.has(PermissionFlagsBits.SendMessages)) {
-				await interaction.editReply({
-					embeds: [errorEmbed],
-				})
-				return
-			}
-
-			channel.permissionOverwrites.create(interaction.guild.id, {
-				SendMessages: true,
-			})
-
-			if (channel === interaction.channel) {
-				await interaction.editReply({
-					embeds: [embed],
-				})
-			}
-			else {
-				await interaction.editReply({ content: '**Unlocked Successfully**' })
-				await channel.send({
-					embeds: [embed],
-				})
-			}
+	  const channel = interaction.options.getChannel('channel') || interaction.channel;
+	  await interaction.deferReply({ ephemeral: true });
+  
+	  // Check if the bot has the required permissions
+	  if (!channel.permissionsFor(interaction.client.user).has(PermissionFlagsBits.ManageChannels)) {
+		const noPermissionEmbed = new EmbedBuilder()
+		  .setTitle('ERROR')
+		  .setColor('Red')
+		  .setDescription('I do not have the required permissions to unlock the channel.');
+		await interaction.editReply({ embeds: [noPermissionEmbed] });
+		return;
+	  }
+  
+	  const embed = new EmbedBuilder()
+		.setTitle(`${channel} has been unlocked`)
+		.setColor('Green')
+		.setFooter({
+		  text: `Done by: ${interaction.user.username}`,
+		  iconURL: `${interaction.user.avatarURL()}`,
+		});
+  
+	  const errorEmbed = new EmbedBuilder()
+		.setTitle('ERROR')
+		.setColor('Red')
+		.setDescription(`${channel} is already unlocked`);
+  
+	  // Check if the channel is already unlocked
+	  if (!channel.permissionOverwrites.cache.get(interaction.guild.id)?.deny.has(PermissionFlagsBits.SendMessages)) {
+		await interaction.editReply({
+		  embeds: [errorEmbed],
+		});
+		return;
+	  }
+  
+	  try {
+		// Unlock the channel
+		await channel.permissionOverwrites.create(interaction.guild.id, {
+		  SendMessages: null,
+		});
+  
+		if (channel === interaction.channel) {
+		  await interaction.editReply({
+			embeds: [embed],
+		  });
+		} else {
+		  await interaction.editReply({ content: '**Unlocked Successfully**' });
+		  await channel.send({
+			embeds: [embed],
+		  });
+		}
+  
+	  } catch (error) {
+		const errorEmbed = new EmbedBuilder()
+		  .setTitle('ERROR')
+		  .setColor('Red')
+		  .setDescription(`An error occurred while trying to unlock the channel: ${error.message}`);
+		await interaction.editReply({ embeds: [errorEmbed] });
+	  }
 	},
-}
+  };
+  
