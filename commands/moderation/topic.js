53e294a4c521f4685df3d1bbb49f14836d673db4
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-06-27 11:33:02 +0530

    mass update

--- commands/moderation/topic.js
+++ commands/moderation/topic.js
@@ -2,18 +2,29 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-    .setName('topic')
-    .setDescription('Sends a message about changing the topic.')
-    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers | PermissionFlagsBits.ManageChannels)
-    .addUserOption((option) => option.setName('user1').setDescription('The user1 to change the topic of.').setRequired(false))
-    .addUserOption((option) => option.setName('user2').setDescription('The user2 to change the topic of.').setRequired(false))
-    .addUserOption((option) => option.setName('user3').setDescription('The user3 to change the topic of.').setRequired(false)),
+        .setName('topic')
+        .setDescription('Sends a message about changing the topic.')
+        .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers | PermissionFlagsBits.ManageChannels)
+        .addUserOption(option => option.setName('user1').setDescription('The first user to change the topic of.').setRequired(false))
+        .addUserOption(option => option.setName('user2').setDescription('The second user to change the topic of.').setRequired(false))
+        .addUserOption(option => option.setName('user3').setDescription('The third user to change the topic of.').setRequired(false)),
     category: 'utility',
     async execute(interaction) {
-        const user1 = interaction.options.getUser('user1') || "";
-        const user2 = interaction.options.getUser('user2') || "";
-        const user3 = interaction.options.getUser('user3') || "";
-        await interaction.reply({ content: 'Done', ephemeral: true });
-        await interaction.channel.send(`${user1}${user2}${user3}\n**Please change the topic immediately. Failing to do so will result in a mute/ban.**`);
+        const user1 = interaction.options.getUser('user1');
+        const user2 = interaction.options.getUser('user2');
+        const user3 = interaction.options.getUser('user3');
+
+        // Build the user mentions string
+        let userMentions = '';
+        if (user1) userMentions += `${user1}\n`;
+        if (user2) userMentions += `${user2}\n`;
+        if (user3) userMentions += `${user3}\n`;
+
+        if (!userMentions) {
+            return interaction.reply({ content: 'No users provided. Please specify at least one user to change the topic.', ephemeral: true });
+        }
+
+        await interaction.reply({ content: 'Topic change message sent.', ephemeral: true });
+        await interaction.channel.send(`${userMentions}\n**Please change the topic immediately. Failing to do so will result in a mute/ban.**`);
     }
-}
+};
