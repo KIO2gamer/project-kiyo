7c3060ba3a1d61f8ddf77e5f1ffe29a1785c00ba
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-06-28 15:21:43 +0530

    updated slowmode.js and reload.js

--- commands/moderation/slowmode.js
+++ commands/moderation/slowmode.js
@@ -1,66 +1,87 @@
 const {
-    SlashCommandBuilder,
-    EmbedBuilder,
-    PermissionFlagsBits,
-    ChannelType,
-  } = require('discord.js');
-  const ms = require('ms');
+  SlashCommandBuilder,
+  EmbedBuilder,
+  PermissionFlagsBits,
+} = require('discord.js');
+const ms = require('ms');
 
-  module.exports = {
-    data: new SlashCommandBuilder()
+module.exports = {
+  data: new SlashCommandBuilder()
       .setName('slowmode')
-      .setDescription('Set slowmode for a channel')
-      .setDMPermission(false)
-      .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+      .setDescription('Set a slowmode for a channel.')
       .addStringOption(option =>
         option
-          .setName('duration')
-          .setDescription('Slowmode duration')
-          .setRequired(true)
-      )
+        .setName('duration')
+        .setDescription('The duration of the slowmode (e.g., 10s, 5m, 1h)')
+        .setRequired(true))
       .addChannelOption(option =>
         option
-          .setName('channel')
-          .setDescription('The channel to set slowmode for')
-          .addChannelTypes(ChannelType.GuildText, ChannelType.GuildAnnouncement)
-          .setRequired(false)
-      ),
-    category: 'moderation',
-    async execute(interaction) {
+        .setName('channel')
+        .setDescription('The channel to set slowmode in')
+        .setRequired(false))
+      .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+      .setDMPermission(false),
+  category: 'moderation',
+  async execute(interaction) {
       const channel = interaction.options.getChannel('channel') || interaction.channel;
-      const durationRaw = interaction.options.getString('duration');
-      const duration = ms(durationRaw);
-  
-      if (!channel.permissionsFor(interaction.client.user).has(PermissionFlagsBits.ManageChannels)) {
-        const noPermissionEmbed = new EmbedBuilder()
-          .setTitle('ERROR')
-          .setColor('Red')
-          .setDescription('I do not have the required permissions to set slowmode for this channel.');
-        await interaction.reply({ embeds: [noPermissionEmbed], ephemeral: true });
-        return;
+      const durationInput = interaction.options.getString('duration');
+      const duration = ms(durationInput) / 1000;
+
+      // Defer the reply to allow time for the operation
+      await interaction.deferReply();
+
+      if (isNaN(duration) || duration < 0 || duration > 21600) {
+          await interaction.editReply({
+              embeds: [
+                  new EmbedBuilder()
+                      .setTitle('ERROR')
+                      .setDescription('Invalid duration. Please provide a duration between 0 seconds and 6 hours.')
+                      .setColor('Red')
+                      .setFooter({
+                          text: `Set by: ${interaction.user.username}`,
+                          iconURL: `${interaction.user.displayAvatarURL()}`,
+                      }),
+              ],
+          });
+          return;
       }
-  
-      if (duration < 0 || duration > 21600) {
-        const invalidDurationEmbed = new EmbedBuilder()
-          .setTitle('ERROR')
-          .setColor('Red')
-          .setDescription('The duration must be between 0 and 21600 seconds (6 hours).');
-        await interaction.reply({ embeds: [invalidDurationEmbed], ephemeral: true });
-        return;
+
+      try {
+          await channel.setRateLimitPerUser(duration);
+
+          let description;
+          if (duration === 0) {
+              description = `The slowmode for <#${channel.id}> has been disabled.`;
+          } else {
+              description = `The slowmode for <#${channel.id}> has been set to ${durationInput}.`;
+          }
+
+          await interaction.editReply({
+              embeds: [
+                  new EmbedBuilder()
+                      .setTitle('Slowmode Set')
+                      .setDescription(description)
+                      .setColor('Green')
+                      .setFooter({
+                          text: `Set by: ${interaction.user.username}`,
+                          iconURL: `${interaction.user.displayAvatarURL()}`,
+                      }),
+              ],
+          });
+      } catch (error) {
+          console.error('Error setting slowmode:', error);
+          await interaction.editReply({
+              embeds: [
+                  new EmbedBuilder()
+                      .setTitle('ERROR')
+                      .setDescription('An error occurred while trying to set the slowmode.')
+                      .setColor('Red')
+                      .setFooter({
+                          text: `Set by: ${interaction.user.username}`,
+                          iconURL: `${interaction.user.displayAvatarURL()}`,
+                      }),
+              ],
+          });
       }
-  
-      await channel.setRateLimitPerUser(duration);
-  
-      const successEmbed = new EmbedBuilder()
-        .setTitle('Slowmode Set')
-        .setColor('Green')
-        .setDescription(`Slowmode for ${channel} has been set to ${duration} seconds.`)
-        .setFooter({
-          text: `Set by: ${interaction.user.username}`,
-          iconURL: `${interaction.user.avatarURL()}`,
-        });
-  
-      await interaction.reply({ embeds: [successEmbed], ephemeral: false });
-    },
-  };
-  
+  },
+};
