2f1c739bf9314d0ca3ec27868cf1da438a54d9ae
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-06-29 17:37:14 +0530

    added poll commands

---
+++ commands/utility/endPoll.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('end_poll')
+        .setDescription('Ends a poll')
+        .addStringOption(option => 
+            option
+                .setName('message_id')
+                .setDescription('Message ID of the poll')
+        )
+        .addChannelOption(option => 
+            option
+                .setName('channel')
+                .setDescription('Channel where the poll is created')
+        ),
+    category: 'utility',
+    async execute(interaction) {
+        const messageId = interaction.options.getString('message_id');
+        const channel = interaction.options.getChannel('channel')
+        const message = await channel.messages.fetch(messageId);
+        message.poll.end();
+        await interaction.reply("Poll Ended Succesfully!!!")
+    }
+}
