7b19266c55c3215926d75bec26bbc26df56495f8
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-06-29 09:28:50 +0530

    added viewRoles.js and logs.js

---
+++ commands/info/viewRoles.js
@@ -0,0 +1,23 @@
+const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+       .setName('viewroles')
+       .setDescription('Shows all the roles in the server.'),
+    category: 'info',
+    async execute(interaction) {
+        const roles = interaction.guild.roles.cache.sort((a, b) => b.position - a.position).map(role => role.toString()).join('\n');
+        await interaction.reply({
+            embeds: [
+                new EmbedBuilder()
+                   .setTitle('Roles')
+                   .setDescription(roles)
+                   .setColor('Orange')
+                   .setFooter({
+                        text: `Requested by: ${interaction.user.username}`,
+                        iconURL: interaction.user.avatarURL(),
+                    }),
+            ],
+        });
+    }
+}
