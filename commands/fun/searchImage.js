2b704517b277ff42ee3b78162a901d893b662d14
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-07-01 23:40:08 +0530

    updated egg.js to searchimage.js

--- commands/fun/searchImage.js
+++ commands/fun/searchImage.js
@@ -1,18 +1,18 @@
 const { SlashCommandBuilder } = require('discord.js');
 require('dotenv').config();
 
-
 module.exports = {
-	data: new SlashCommandBuilder().setName('egg').setDescription('send egg image'),
+	data: new SlashCommandBuilder().setName('searchimage').setDescription('sends a image').addStringOption(option => option.setName('query').setDescription('search query').setRequired(true)),
 	category: 'fun',
 	async execute(interaction) {
+        const query = interaction.options.getString('query');
 		try {
-            const response = await fetch(`https://api.unsplash.com/photos/random?query=interesting%20egg&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
+            const response = await fetch(`https://api.unsplash.com/photos/random?query=${query.replace(/ /g, '%20')}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
             const data = await response.json();
             if (data && data.urls && data.urls.regular) {
                 await interaction.reply(data.urls.regular);
             } else {
-                await interaction.reply('Sorry, I couldn\'t find any egg images.');
+                await interaction.reply('Sorry, I couldn\'t find any images.');
             }
         } catch (error) {
             console.error(error);
