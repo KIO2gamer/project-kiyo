df5d0c52dad4226ccfe556312f61f87618ad86cd
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-07-03 23:59:34 +0530

    added welcome event

--- commands/fun/searchImage.js
+++ commands/fun/searchImage.js
@@ -2,21 +2,28 @@ const { SlashCommandBuilder } = require('discord.js');
 require('dotenv').config();
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('searchimage').setDescription('sends a image').addStringOption(option => option.setName('query').setDescription('search query').setRequired(true)),
+	data: new SlashCommandBuilder()
+		.setName('searchimage')
+		.setDescription('sends a image')
+		.addStringOption(option =>
+			option.setName('query').setDescription('search query').setRequired(true)
+		),
 	category: 'fun',
 	async execute(interaction) {
-        const query = interaction.options.getString('query');
+		const query = interaction.options.getString('query');
 		try {
-            const response = await fetch(`https://api.unsplash.com/photos/random?query=${query.replace(/ /g, '%20')}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`);
-            const data = await response.json();
-            if (data && data.urls && data.urls.regular) {
-                await interaction.reply(data.urls.regular);
-            } else {
-                await interaction.reply('Sorry, I couldn\'t find any images.');
-            }
-        } catch (error) {
-            console.error(error);
-            await interaction.reply('An error occurred while fetching the image.');
-        }
+			const response = await fetch(
+				`https://api.unsplash.com/photos/random?query=${query.replace(/ /g, '%20')}&client_id=${process.env.UNSPLASH_ACCESS_KEY}`
+			);
+			const data = await response.json();
+			if (data && data.urls && data.urls.regular) {
+				await interaction.reply(data.urls.regular);
+			} else {
+				await interaction.reply("Sorry, I couldn't find any images.");
+			}
+		} catch (error) {
+			console.error(error);
+			await interaction.reply('An error occurred while fetching the image.');
+		}
 	},
 };
--- commands/info/minecraftInfo.js
+++ commands/info/minecraftInfo.js
@@ -3,59 +3,65 @@ const NodeCache = require('node-cache');
 const cache = new NodeCache({ stdTTL: 300, checkperiod: 60 }); // Cache for 5 minutes
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('minecraft')
-        .setDescription('Sends info about a Minecraft player')
-        .addStringOption(option => option
-            .setName('username')
-            .setDescription('Search for a Minecraft player')
-            .setRequired(true)),
-    category: 'fun',
-    async execute(interaction) {
-        const username = interaction.options.getString('username');
+	data: new SlashCommandBuilder()
+		.setName('minecraft')
+		.setDescription('Sends info about a Minecraft player')
+		.addStringOption(option =>
+			option
+				.setName('username')
+				.setDescription('Search for a Minecraft player')
+				.setRequired(true)
+		),
+	category: 'fun',
+	async execute(interaction) {
+		const username = interaction.options.getString('username');
 
-        // Check if the data is in cache
-        const cachedData = cache.get(username);
-        if (cachedData) {
-            await interaction.reply({ embeds: [createEmbed(cachedData)] });
-            return;
-        }
+		// Check if the data is in cache
+		const cachedData = cache.get(username);
+		if (cachedData) {
+			await interaction.reply({ embeds: [createEmbed(cachedData)] });
+			return;
+		}
 
-        try {
-            const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`);
-            const data = await response.json();
+		try {
+			const response = await fetch(
+				`https://api.mojang.com/users/profiles/minecraft/${username}`
+			);
+			const data = await response.json();
 
-            if (data && data.id && data.name) {
-                const skinUrl = `https://minotar.net/helm/${data.id}/256.png`;
+			if (data && data.id && data.name) {
+				const skinUrl = `https://minotar.net/helm/${data.id}/256.png`;
 
-                const playerData = {
-                    name: data.name,
-                    uuid: data.id,
-                    skinUrl: skinUrl
-                };
+				const playerData = {
+					name: data.name,
+					uuid: data.id,
+					skinUrl: skinUrl,
+				};
 
-                // Cache the data
-                cache.set(username, playerData);
+				// Cache the data
+				cache.set(username, playerData);
 
-                await interaction.reply({ embeds: [createEmbed(playerData)] });
-            } else {
-                await interaction.reply('Could not find the Minecraft player.');
-            }
-        } catch (error) {
-            console.error(error);
-            await interaction.reply('An error occurred while fetching the player information. Please try again later.');
-        }
-    },
+				await interaction.reply({ embeds: [createEmbed(playerData)] });
+			} else {
+				await interaction.reply('Could not find the Minecraft player.');
+			}
+		} catch (error) {
+			console.error(error);
+			await interaction.reply(
+				'An error occurred while fetching the player information. Please try again later.'
+			);
+		}
+	},
 };
 
 function createEmbed(playerData) {
-    return new EmbedBuilder()
-        .setColor('#0099ff')
-        .setTitle(`Minecraft Player: ${playerData.name}`)
-        .setThumbnail(playerData.skinUrl)
-        .addFields(
-            { name: 'Username', value: playerData.name, inline: true },
-            { name: 'UUID', value: playerData.uuid, inline: true }
-        )
-        .setTimestamp();
+	return new EmbedBuilder()
+		.setColor('#0099ff')
+		.setTitle(`Minecraft Player: ${playerData.name}`)
+		.setThumbnail(playerData.skinUrl)
+		.addFields(
+			{ name: 'Username', value: playerData.name, inline: true },
+			{ name: 'UUID', value: playerData.uuid, inline: true }
+		)
+		.setTimestamp();
 }
