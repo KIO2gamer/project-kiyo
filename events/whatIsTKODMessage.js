3e33306974c21727be67e71780df8168fb6d3624
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-07-06 14:07:22 +0530

    cxv

--- events/whatIsTKODMessage.js
+++ events/whatIsTKODMessage.js
@@ -5,7 +5,7 @@ module.exports = {
 	once: false,
 	async execute(message, client) {
         if (message.author.bot) return;
-        if (message.content === 'TKOD' || "tkod") {
+        if (message.content === 'TKOD' || 'tkod') {
             message.channel.send('**T** - The\n**K** - KIO2gamer\n**O** - Official\n**D** - Discord (Server)');
         }
 	},
