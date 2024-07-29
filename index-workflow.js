f2a92ced2c37463eb5a3edf1f9bd0146f583862c
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-07-29 11:08:54 +0530

    updated

---
+++ index-workflow.js
@@ -0,0 +1,13 @@
+const discord = require('discord.js');
+const client = new discord.Client();
+
+client.once('ready', () => {
+    console.log('Bot is online for workflow check!');
+    // Perform any startup checks or tasks
+    setTimeout(() => {
+        console.log('Exiting workflow bot');
+        process.exit(0);
+    }, 5 * 1000); // Adjust the timeout as needed, here it's 5 seconds
+});
+
+client.login(process.env.DISCORD_TOKEN);
