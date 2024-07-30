c52d3fb19d1135d5f6de0d76d7f0a1f78d91ce51
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-07-30 16:00:03 +0530

    updated quokka

--- commands/fun/quokka.js
+++ commands/fun/quokka.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -6,6 +6,9 @@ module.exports = {
 		.setDescription('Send a pic of a quokka because it is cute.'),
 	category: 'fun',
 	async execute(interaction) {
+		// Defer the reply to give more time for processing
+		await interaction.deferReply();
+
 		try {
 			const response = await fetch(
 				`https://tenor.googleapis.com/v2/search?q=quokka&key=${process.env.TENOR_API_KEY}&limit=10`
@@ -14,16 +17,19 @@ module.exports = {
 
 			if (data.results && data.results.length > 0) {
 				const randomIndex = Math.floor(Math.random() * data.results.length);
-				const quokkaGif = data.results[randomIndex].url;
-				await interaction.reply(
-					`***You have been blessed by the powers of a quokka***\n${quokkaGif}`
-				);
+				const quokkaGif = data.results[randomIndex].media_formats.gif.url; // Adjust as necessary for the correct URL
+
+				const embed = new EmbedBuilder()
+					.setTitle('You have been blessed by the powers of a quokka!')
+					.setImage(quokkaGif);
+
+				await interaction.editReply({ embeds: [embed] });
 			} else {
-				await interaction.reply('Sorry, I could not find a quokka GIF.');
+				await interaction.editReply('Sorry, I could not find a quokka GIF.');
 			}
 		} catch (error) {
 			console.error('Error fetching quokka GIF:', error);
-			await interaction.reply('There was an error trying to fetch a quokka GIF.');
+			await interaction.editReply('There was an error trying to fetch a quokka GIF.');
 		}
 	},
 };
