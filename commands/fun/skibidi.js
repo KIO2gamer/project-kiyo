50c011929959cb24fbaf5b61a6b52167a815a234
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-08-01 13:59:05 +0530

    updated credits

--- commands/fun/skibidi.js
+++ commands/fun/skibidi.js
@@ -1,4 +1,5 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -7,20 +8,24 @@ module.exports = {
 		.addStringOption(option =>
 			option
 				.setName('option')
-				.setDescription('Did you watched skibidi toilet?')
-				.addChoices({ name: 'Yes', value: 'yes' }, { name: 'No', value: 'no' })
+				.setDescription('Did you watch Skibidi Toilet?')
+				.addChoices(
+					{ name: 'Yes', value: 'yes' },
+					{ name: 'No', value: 'no' }
+				)
 				.setRequired(true)
 		),
 	category: 'fun',
 	async execute(interaction) {
 		const option = interaction.options.getString('option');
 		if (option === 'yes') {
-			await interaction.reply(
-				'https://tenor.com/view/eeeeeehmazin-ehmazing-amazing-gif-1173311831093611344'
-			);
-			await interaction.channel.send('**Skibidi powers activated successfully ✅**');
+			const embed = new EmbedBuilder()
+				.setDescription('## Skibidi powers activated successfully ✅')
+				.setImage('https://tenor.com/view/eeeeeehmazin-ehmazing-amazing-gif-1173311831093611344')
+				.setColor('#00FF00'); // Optional: Add a color to the embed
+			await interaction.reply({ embeds: [embed] });
 		} else {
-			await interaction.reply('**You are not worthy enough to yield the powers, mortal**');
+			await interaction.reply('***You are not worthy enough to wield the powers, mortal***');
 		}
 	},
 };
