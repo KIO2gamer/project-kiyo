f3e03466d112f2bf7b43f2bf9ddbe74a73c364dc
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-08-08 09:17:03 +0530

    updated

--- commands/fun/skibidi.js
+++ commands/fun/skibidi.js
@@ -17,28 +17,10 @@ module.exports = {
 		const option = interaction.options.getString('option');
 
 		if (option === 'yes') {
-			await interaction.deferReply();
-			try {
-				const response = await fetch(
-					`https://tenor.googleapis.com/v2/search?q=nickeh30ehmazing&key=${process.env.TENOR_API_KEY}&limit=1`
-				);
-				const data = await response.json();
-				const url = data.results[0].url;
-
-				const embed = new EmbedBuilder()
-					.setDescription('## Skibidi powers activated successfully ✅')
-					.setImage(url)
-					.setColor('#00FF00'); // Optional: Add a color to the embed
-
-				await interaction.editReply({ embeds: [embed] });
-			} catch (error) {
-				console.error('Error fetching GIF:', error);
-				await interaction.editReply(
-					'***Failed to fetch Skibidi powers GIF. Please try again later.***'
-				);
-			}
+			await interaction.reply('Skibidi powers activated successfully ✅');
+			await interaction.channel.send('https://tenor.com/view/eeeeeehmazin-ehmazing-amazing-gif-1173311831093611344' )
 		} else {
-			await interaction.editReply(
+			await interaction.reply(
 				'***You are not worthy enough to wield the powers, mortal***'
 			);
 		}
