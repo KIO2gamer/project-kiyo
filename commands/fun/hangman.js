c91941ab47a307a9cffb4c7280741e8a59c21985
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-08-11 15:10:18 +0530

    updated

--- commands/fun/hangman.js
+++ commands/fun/hangman.js
@@ -1,98 +1,135 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
-const path = require('path');
+
+// Wikimedia URLs for hangman images
+const hangmanImages = [
+	'https://upload.wikimedia.org/wikipedia/commons/8/8b/Hangman-0.png',
+	'https://upload.wikimedia.org/wikipedia/commons/3/37/Hangman-1.png',
+	'https://upload.wikimedia.org/wikipedia/commons/7/70/Hangman-2.png',
+	'https://upload.wikimedia.org/wikipedia/commons/9/97/Hangman-3.png',
+	'https://upload.wikimedia.org/wikipedia/commons/2/27/Hangman-4.png',
+	'https://upload.wikimedia.org/wikipedia/commons/6/6b/Hangman-5.png',
+	'https://upload.wikimedia.org/wikipedia/commons/d/d6/Hangman-6.png',
+];
 
 module.exports = {
-	data: new SlashCommandBuilder().setName('hangman').setDescription('Play a game of Hangman!'),
+	data: new SlashCommandBuilder().setName('hangman').setDescription('Start a game of hangman!'),
 	async execute(interaction) {
-		const wordToGuess = getWordToGuess();
-		let wordDisplay = '_ '.repeat(wordToGuess.length).trim();
-		let incorrectGuesses = 0;
-		const maxIncorrectGuesses = 6;
-		const guessedLetters = [];
-
-		let embed = new EmbedBuilder()
-			.setColor('#0099ff')
-			.setTitle('Hangman')
-			.setDescription(`Word: ${wordDisplay}`)
-			.addFields(
-				{ name: 'Incorrect Guesses', value: `${incorrectGuesses}/${maxIncorrectGuesses}` },
-				{
-					name: 'Guessed Letters',
-					value: guessedLetters.length > 0 ? guessedLetters.join(', ') : 'None',
-				}
-			);
-
-		await interaction.reply({ embeds: [embed] });
-
-		const filter = m => !m.author.bot && m.content.length === 1 && /^[a-zA-Z]$/.test(m.content);
-		const collector = interaction.channel.createMessageCollector({ filter, time: 60000 });
-
-		collector.on('collect', async msg => {
-			const guess = msg.content.toLowerCase();
-
-			if (guessedLetters.includes(guess)) {
-				await msg.reply('You already guessed that letter!');
+		fs.readFile('./assets/texts/hangmanWords.txt', 'utf-8', async (err, data) => {
+			if (err) {
+				console.error('Failed to read the word list:', err);
+				interaction.reply('An error occurred while loading the words.');
 				return;
 			}
 
-			guessedLetters.push(guess);
+			const words = data
+				.split('\n')
+				.map(word => word.trim())
+				.filter(word => word);
 
-			if (wordToGuess.includes(guess)) {
-				let updatedWordDisplay = '';
-				for (let i = 0; i < wordToGuess.length; i++) {
-					if (wordToGuess[i] === guess || wordDisplay[i * 2] !== '_') {
-						updatedWordDisplay += wordDisplay[i * 2] + ' ';
-					} else {
-						updatedWordDisplay += '\_';
-					}
-				}
-				updatedWordDisplay = updatedWordDisplay.trim();
+			if (words.length === 0) {
+				interaction.reply('The word list is empty!');
+				return;
+			}
 
-				embed.setDescription(`Word: ${updatedWordDisplay}`);
-				await msg.reply({ embeds: [embed] });
+			const word = words[Math.floor(Math.random() * words.length)];
+			const guessedLetters = [];
+			let remainingGuesses = 6;
+			let wordState = '_ '.repeat(word.length).trim();
+			let hintsUsed = 0;
 
-				if (updatedWordDisplay.replace(/\s/g, '') === wordToGuess) {
-					collector.stop('won');
-				}
-			} else {
-				incorrectGuesses++;
-				embed.setFields(
+			const gameEmbed = new EmbedBuilder()
+				.setColor(0x0099ff)
+				.setTitle('Hangman')
+				.setDescription(`\`\`\`${wordState}\`\`\``)
+				.setThumbnail(hangmanImages[0])
+				.addFields(
+					{ name: 'Guesses Left', value: remainingGuesses.toString(), inline: true },
 					{
-						name: 'Incorrect Guesses',
-						value: `${incorrectGuesses}/${maxIncorrectGuesses}`,
+						name: 'Guessed Letters',
+						value: guessedLetters.join(', ') || 'None',
+						inline: true,
 					},
-					{ name: 'Guessed Letters', value: guessedLetters.join(', ') }
+					{ name: 'Hints Used', value: hintsUsed.toString(), inline: true }
 				);
-				await msg.reply({ embeds: [embed] });
 
-				if (incorrectGuesses >= maxIncorrectGuesses) {
-					collector.stop('lost');
+			const msg = await interaction.reply({ embeds: [gameEmbed] });
+
+			const filter = m => m.author.id === interaction.user.id;
+			const collector = interaction.channel.createMessageCollector({ filter, time: 60000 });
+
+			collector.on('collect', async guess => {
+				const content = guess.content.toLowerCase();
+
+				if (content.startsWith('!hint') && hintsUsed < 3) {
+					hintsUsed++;
+					// Implement hint logic here
+					// ... provide a hint based on the word ...
+				} else if (/^[a-zA-Z]$/.test(content)) {
+					const letter = content;
+
+					if (guessedLetters.includes(letter)) {
+						await guess.reply('You already guessed that letter!');
+						return;
+					}
+
+					guessedLetters.push(letter);
+
+					if (word.includes(letter)) {
+						for (let i = 0; i < word.length; i++) {
+							if (word[i] === letter) {
+								wordState =
+									wordState.substring(0, i * 2) +
+									letter +
+									wordState.substring(i * 2 + 1);
+							}
+						}
+					} else {
+						remainingGuesses--;
+					}
+				} else {
+					await guess.reply('Please enter a single letter or use "!hint" for a hint.');
+					return;
 				}
-			}
-		});
 
-		collector.on('end', (collected, reason) => {
-			if (reason === 'won') {
-				embed.setDescription(`You won! The word was **${wordToGuess}**`);
-				embed.setColor('#00FF00');
-				interaction.followUp({ embeds: [embed] });
-			} else if (reason === 'lost') {
-				embed.setDescription(`You lost! The word was **${wordToGuess}**`);
-				embed.setColor('#FF0000');
-				interaction.followUp({ embeds: [embed] });
-			} else {
-				embed.setDescription(`Time's up! The word was **${wordToGuess}**`);
-				interaction.followUp({ embeds: [embed] });
-			}
+				gameEmbed
+					.setDescription(`\`\`\`${wordState}\`\`\``)
+					.setThumbnail(hangmanImages[6 - remainingGuesses])
+					.setFields(
+						{ name: 'Guesses Left', value: remainingGuesses.toString(), inline: true },
+						{ name: 'Guessed Letters', value: guessedLetters.join(', '), inline: true },
+						{ name: 'Hints Used', value: hintsUsed.toString(), inline: true }
+					);
+
+				if (remainingGuesses === 0) {
+					gameEmbed
+						.setColor(0xff0000)
+						.setTitle('You Lose!')
+						.setDescription(`The word was **${word}**`);
+					await msg.edit({ embeds: [gameEmbed] });
+					collector.stop();
+					return;
+				}
+
+				if (wordState.replace(/ /g, '') === word) {
+					gameEmbed.setColor(0x00ff00).setTitle('You Win!');
+					await msg.edit({ embeds: [gameEmbed] });
+					collector.stop();
+					return;
+				}
+
+				await msg.edit({ embeds: [gameEmbed] });
+			});
+
+			collector.on('end', (collected, reason) => {
+				if (reason === 'time') {
+					gameEmbed
+						.setColor(0xff0000)
+						.setTitle('Time Up!')
+						.setDescription(`The word was **${word}**`);
+					msg.edit({ embeds: [gameEmbed] });
+				}
+			});
 		});
 	},
 };
-
-function getWordToGuess() {
-	const wordListFile = path.join('./assets/texts/hangmanWords.txt');
-	const fileContent = fs.readFileSync(wordListFile, 'utf-8');
-	const words = fileContent.split('\n');
-	const randomIndex = Math.floor(Math.random() * words.length);
-	return words[randomIndex];
-}
