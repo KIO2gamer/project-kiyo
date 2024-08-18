67b4b835a4a760d9262c0862fa14d2b2ad2c77d7
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-08-19 00:14:16 +0530

    updated

--- commands/admin_or_dev/deleteRoleFromJSON.js
+++ commands/admin_or_dev/deleteRoleFromJSON.js
@@ -1,12 +1,51 @@
 const { SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('delete_role_from_data')
-		.setDescription('Adds a role to the json file data.')
+		.setDescription('Deletes a role from the json file data.')
 		.addRoleOption(option =>
-			option.setName('role').setDescription('The role to add').setRequired(true)
+			option.setName('role').setDescription('The role to delete').setRequired(true)
 		),
+	async execute(interaction) {
+		const role = interaction.options.getRole('role');
 
-	async execute(interaction) {},
+		fs.readFile('./assets/json/roles.json', 'utf8', (err, data) => {
+			if (err) {
+				console.error(err);
+				return interaction.reply('An error occurred while reading the file.');
+			}
+
+			let jsonData = {};
+			try {
+				jsonData = JSON.parse(data);
+			} catch (parseError) {
+				console.warn(
+					'File was empty or contained invalid JSON. Starting with an empty object.'
+				);
+			}
+
+			if (!jsonData.roles) {
+				return interaction.reply('There are no roles in the data!');
+			}
+
+			const roleIndex = jsonData.roles.findIndex(
+				existingRole => existingRole.roleID === role.id
+			);
+			if (roleIndex === -1) {
+				return interaction.reply(`The role "${role.name}" was not found in the data!`);
+			}
+
+			jsonData.roles.splice(roleIndex, 1);
+
+			fs.writeFile('./assets/json/roles.json', JSON.stringify(jsonData, null, 2), err => {
+				if (err) {
+					console.error(err);
+					return interaction.reply('An error occurred while writing to the file.');
+				}
+				interaction.reply(`Role "${role.name}" has been deleted from the data!`);
+			});
+		});
+	},
 };
---
+++ commands/admin_or_dev/viewRolesFromJSON.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+const fs = require('fs');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('view_roles_from_data')
+		.setDescription('View the roles stored in the data.'),
+	async execute(interaction) {
+		fs.readFile('./assets/json/roles.json', 'utf8', (err, data) => {
+			if (err) {
+				console.error(err);
+				return interaction.reply('An error occurred while reading the role data.');
+			}
+
+			let jsonData = {};
+			try {
+				jsonData = JSON.parse(data);
+			} catch (parseError) {
+				console.warn('File was empty or contained invalid JSON.');
+			}
+
+			if (!jsonData.roles || jsonData.roles.length === 0) {
+				return interaction.reply('There are no roles stored in the data.');
+			}
+
+			const embed = new EmbedBuilder()
+				.setTitle('Stored Roles')
+				.setColor('#00FFFF')
+				.setDescription(
+					jsonData.roles
+						.map(
+							(role, index) =>
+								`${index + 1}. **${role.roleName}** (ID: \`${role.roleID}\`, Color: \`${role.roleColor}\`)`
+						)
+						.join('\n')
+				);
+
+			interaction.reply({ embeds: [embed] });
+		});
+	},
+};
