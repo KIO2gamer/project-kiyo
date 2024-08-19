dd9b15d338668c214db666f072d84b54cab6fa71
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-08-19 08:43:57 +0530

    mass updated

---
+++ commands/channel_settings/deleteChannel.js
@@ -0,0 +1,37 @@
+const {
+	SlashCommandBuilder,
+	ChannelType,
+	PermissionFlagsBits,
+	EmbedBuilder,
+} = require('discord.js');
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('deletechannel')
+		.setDescription('Deletes a specified channel.')
+		.setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels)
+		.addChannelOption(option =>
+			option.setName('channel').setDescription('The channel to delete').setRequired(true)
+		),
+	async execute(interaction) {
+		const channel = interaction.options.getChannel('channel');
+
+		try {
+			await channel.delete();
+
+			const embed = new EmbedBuilder()
+				.setTitle('Channel Deleted!')
+				.setColor('Red')
+				.setDescription(`The channel <#${channel.id}> has been successfully deleted.`)
+				.setTimestamp();
+
+			await interaction.reply({ embeds: [embed] });
+		} catch (error) {
+			console.error(`Error deleting channel: ${error}`);
+			await interaction.reply({
+				content: 'An error occurred while deleting the channel.',
+				ephemeral: true,
+			});
+		}
+	},
+};
