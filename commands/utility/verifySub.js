61deba5fe74bbf3eb99cd5ba046c3860a1708b0e
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-09-20 01:02:22 +0530

    updated

--- commands/utility/verifySub.js
+++ commands/utility/verifySub.js
@@ -1,53 +1,21 @@
-const { SlashCommandBuilder } = require('discord.js')
-const axios = require('axios')
-const qs = require('qs')
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 const { clientId, redirectUri } = require('../../discord-oauth2/config.json')
 
 module.exports = {
+    description_full:
+        'Verifies a user by checking if they have a YouTube channel linked to their Discord account. If the user has a YouTube channel linked, they are verified and can access the bot. If not, they are not verified and cannot access the bot.',
+    usage: '/verify',
     data: new SlashCommandBuilder()
         .setName('verify')
         .setDescription(
             'Verifies a user by checking if they have a YouTube channel linked to their Discord account.'
         ),
     async execute(interaction) {
-        const authUrl = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify+connections`
+        const authURL = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&response_type=code&scope=identify+connections`
+        const embed = new EmbedBuilder()
+            .setTitle('Verify Your Account')
+            .setDescription(`Click [here](${authURL}) to verify your account.`)
 
-        try {
-            const response = await axios.get(authUrl, {
-                headers: {
-                    Authorization: `Bot ${interaction.client.token}`,
-                },
-            })
-
-            const authCode = qs.parse(response.data.split('&')[1]).code
-
-            if (authCode) {
-                const authLoginUrl =
-                    'https://discord-oauth2.netlify.app/.netlify/functions/authLogin'
-
-                try {
-                    const authLoginResponse = await axios.post(authLoginUrl, {
-                        code: authCode,
-                    })
-                    const youtubeId = authLoginResponse.data.youtubeId
-
-                    if (youtubeId) {
-                        await interaction.reply(`Your YouTube ID: ${youtubeId}`)
-                    } else {
-                        await interaction.reply(
-                            'You do not have a YouTube channel linked to your Discord account.'
-                        )
-                    }
-                } catch (error) {
-                    console.error('Error in authLogin.js:', error)
-                    await interaction.reply('Failed to get YouTube ID.')
-                }
-            } else {
-                await interaction.reply('Failed to fetch authorization code.')
-            }
-        } catch (error) {
-            console.error('Error in verifySub.js:', error)
-            await interaction.reply('Failed to authorize.')
-        }
+        await interaction.reply({ embeds: [embed] })
     },
 }
