9c5b2b284f7d136e528187959ac5ba4b77e571b8
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-09-20 12:29:16 +0530

    updated

--- commands/utility/verifySub.js
+++ commands/utility/verifySub.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
 const { redirectUri } = require('../../discord-oauth2/config.json')
-
+const eventEmitter = require('../../discord-oauth2/eventEmitter') // Adjust path if needed
 module.exports = {
     description_full:
         'Verifies a user by checking if they have a YouTube channel linked to their Discord account. If the user has a YouTube channel linked, they are verified and can access the bot. If not, they are not verified and cannot access the bot.',
@@ -11,18 +11,23 @@ module.exports = {
             'Verifies a user by checking if they have a YouTube channel linked to their Discord account.'
         ),
     async execute(interaction) {
-        
-        let description = ``
-        if (getYoutubeId()) {
-            description = `You are verified! You can now access the bot.\nYoutube Channel ID: ${getYoutubeId()}`
-        }
-        else {
-            description = `You are not verified. Please link your YouTube channel to your Discord account by clicking [here](${redirectUri}).`
-        }
-
         const embed = new EmbedBuilder()
             .setTitle('Verify Your Account')
-            .setDescription(description)
+            .setDescription(
+                `Please link your YouTube channel to your Discord account by clicking [here](${redirectUri}).`
+            )
+
+        eventEmitter.on('youtubeId', (youtubeId) => {
+            if (youtubeId) {
+                // User is verified, proceed with your logic
+                console.log('User is verified:', youtubeId)
+                // ...
+            } else {
+                // User is not verified, handle accordingly
+                console.log('User is not verified.')
+                // ...
+            }
+        })
 
         await interaction.reply({ embeds: [embed] })
     },
