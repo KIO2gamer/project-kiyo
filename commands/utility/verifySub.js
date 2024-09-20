9fbf99425542f1e6d80607d0514c7e35d6fee62b
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-09-20 12:08:43 +0530

    updated

--- commands/utility/verifySub.js
+++ commands/utility/verifySub.js
@@ -1,9 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
-const mongoose = require('mongoose')
-const User = require('../../discord-oauth2/UserConnections') // Import the User model
-
-// MongoDB connection string from environment variables
-const mongoUri = process.env.MONGODB_URL
+const { redirectUri } = require('../../discord-oauth2/config.json')
+const { getYoutubeId } = require('../../discord-oauth2/youtubeIdFetcher')
 
 module.exports = {
     description_full:
@@ -15,20 +12,13 @@ module.exports = {
             'Verifies a user by checking if they have a YouTube channel linked to their Discord account.'
         ),
     async execute(interaction) {
-        // Connect to MongoDB using Mongoose
-        await mongoose.connect(mongoUri, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        })
-
-        const discordUserId = interaction.user.id // Get the Discord user's ID
-        const youtubeId = await getYoutubeId(discordUserId)
-
-        let description = ''
-        if (youtubeId) {
-            description = `You have successfully linked your YouTube account. Your YouTube Channel ID is: ${youtubeId}`
-        } else {
-            description = `You haven't linked your YouTube account yet. Please click [here](${redirectUri}) to verify your account.`
+        
+        let description = ``
+        if (getYoutubeId()) {
+            description = `You are verified! You can now access the bot.\nYoutube Channel ID: ${getYoutubeId()}`
+        }
+        else {
+            description = `You are not verified. Please link your YouTube channel to your Discord account by clicking [here](${redirectUri}).`
         }
 
         const embed = new EmbedBuilder()
@@ -36,19 +26,5 @@ module.exports = {
             .setDescription(description)
 
         await interaction.reply({ embeds: [embed] })
-
-        // Close the MongoDB connection
-        mongoose.connection.close()
     },
-}
-
-// Fetch YouTube ID from MongoDB using Mongoose
-async function getYoutubeId(discordUserId) {
-    try {
-        const user = await User.findOne({ discordUserId })
-        return user ? user.youtubeId : null
-    } catch (err) {
-        console.error('Error retrieving user from MongoDB:', err)
-        return null
-    }
 }
