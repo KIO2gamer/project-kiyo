aed14de789512f7f3ecc3df3814da1e545030ca1
Utsab Sengupta <utsabsengupta4@gmail.com>
2024-09-26 11:26:05 +0530

    updated

--- commands/roles/roleinfo.js
+++ commands/roles/roleinfo.js
@@ -1,4 +1,4 @@
-const { SlashCommandBuilder } = require('@discordjs/builders')
+const { SlashCommandBuilder } = require('discord.js')
 const { EmbedBuilder, PermissionsBitField } = require('discord.js')
 
 module.exports = {
