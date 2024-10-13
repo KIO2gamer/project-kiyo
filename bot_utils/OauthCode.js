f3a7307acc0441ddd8235902f8edcd451fb5aefa
KIO2gamer <utsabsengupta4@gmail.com>
2024-10-14 00:29:58 +0530

    updated asjpikasdhjfpkoaj

--- bot_utils/OauthCode.js
+++ bot_utils/OauthCode.js
@@ -1,9 +1,11 @@
 const mongoose = require('mongoose');
 
 const oauthCodeSchema = new mongoose.Schema({
-    interactionId: String,
-    code: String,
+    interactionId: { type: String, required: true },
+    code: { type: String, required: true },
+    youtubeChannelId: { type: String }, // Field to store YouTube channel ID
     createdAt: { type: Date, default: Date.now, expires: 600 }, // Expires in 10 minutes
 });
 
-module.exports = mongoose.model('OAuthCode', oauthCodeSchema);
+const OAuthCode = mongoose.model('OAuthCode', oauthCodeSchema);
+module.exports = OAuthCode;
