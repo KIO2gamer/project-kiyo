541e504af9959b3a0f17190f8e462dc1af1dc07c
KIO2gamer <utsabsengupta4@gmail.com>
2024-10-13 19:21:13 +0530

    updated

--- netlify/functions/handleOauth.js
+++ netlify/functions/handleOauth.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const OAuthCode = require('./../../bot_utils/OauthCode');
 
 // MongoDB connection URI from environment variables
-const mongoUri = process.env.MONGODB_URI
+const mongoUri = process.env.MONGODB_URI;
 
 // Initialize Mongoose connection (ensure only one connection is made)
 let isConnected = false; // Keep track of connection status
@@ -10,7 +10,11 @@ let isConnected = false; // Keep track of connection status
 async function connectToDatabase() {
     if (!isConnected) {
         try {
-            await mongoose.connect(mongoUri);
+            await mongoose.connect(mongoUri, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+                bufferCommands: false, // Disable Mongoose buffering
+            });
             isConnected = true;
             console.log('MongoDB connected');
         } catch (error) {
