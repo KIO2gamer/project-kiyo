46e69e88d81eee14cd5b944392fa273a63b59fcf
KIO2gamer <utsabsengupta4@gmail.com>
2024-10-15 11:43:00 +0530

    added reminder feature

---
+++ commands/utility/reminder.js
@@ -0,0 +1,56 @@
+const { SlashCommandBuilder } = require('discord.js');
+const Reminder = require('./../../bot_utils/reminderStorage');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('reminder')
+        .setDescription('Set a reminder after a specified time')
+        .addIntegerOption((option) =>
+            option
+                .setName('time')
+                .setDescription(
+                    'Time in seconds after which you want to be reminded',
+                )
+                .setRequired(true),
+        )
+        .addStringOption((option) =>
+            option
+                .setName('message')
+                .setDescription('Message to be reminded of')
+                .setRequired(true),
+        ),
+
+    async execute(interaction) {
+        const time = interaction.options.getInteger('time'); // Time in seconds
+        const reminderMessage = interaction.options.getString('message');
+        const userId = interaction.user.id;
+        const channelId = interaction.channel.id;
+
+        // Calculate when the reminder should be sent
+        const reminderTime = new Date(Date.now() + time * 1000);
+
+        // Save the reminder in MongoDB
+        const reminder = new Reminder({
+            userId,
+            reminderMessage,
+            reminderTime,
+            channelId,
+        });
+
+        await reminder.save();
+
+        // Confirm that the reminder is set
+        await interaction.reply(
+            `Reminder set! I'll remind you in ${time} seconds: "${reminderMessage}"`,
+        );
+
+        // Schedule the reminder
+        setTimeout(async () => {
+            const channel = await interaction.client.channels.fetch(channelId);
+            await channel.send(`⏰ <@${userId}> Reminder: ${reminderMessage}`);
+
+            // After sending the reminder, delete it from the database
+            await Reminder.findByIdAndDelete(reminder._id);
+        }, time * 1000);
+    },
+};
