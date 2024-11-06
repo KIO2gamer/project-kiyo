db13b75566de6907fb18f510559f9d2a69f35bc7
KIO2gamer <utsabsengupta4@gmail.com>
2024-11-06 20:27:22 +0530

    Refactor GitHub Actions workflow and package.json

--- test/helloWorld.test.js
+++
@@ -1,8 +0,0 @@
-const assert = require('assert');
-
-describe('Hello World', () => {
-    it('should return "Hello, World!"', () => {
-        const result = 'Hello, World!';
-        assert.strictEqual(result, 'Hello, World!');
-    });
-});
