63bee908ca065e2eda03cffbebbba28479607fc5
KIO2gamer <utsabsengupta4@gmail.com>
2024-11-23 15:06:06 +0530

    Remove console.log statement for cleaner event handling

--- src/bot/events/ready.js
+++ src/bot/events/ready.js
@@ -36,11 +36,6 @@ module.exports = {
 					`- ${guild.name} (ID: ${guild.id})`,
 				);
 			});
-
-			console.log(
-				'\x1b[32m%s\x1b[0m',
-				'Bot is fully operational and ready to serve!',
-			);
 		} catch (error) {
 			console.error(
 				'\x1b[31m%s\x1b[0m',
