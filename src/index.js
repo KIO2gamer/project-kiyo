fbc571c6448c069f98fc689251f0f61d8d1b58d7
KIO2gamer <utsabsengupta4@gmail.com>
2024-11-23 15:10:01 +0530

    Remove console.log statement for cleaner bot startup

--- src/index.js
+++ src/index.js
@@ -188,7 +188,6 @@ process.on('SIGINT', async () => {
 		await connectToMongoDB();
 		await deployCommands();
 		await client.login(DISCORD_TOKEN);
-		console.log('\x1b[32m%s\x1b[0m', '[BOT] Bot is running!'); // Green color for success
 	} catch (error) {
 		console.error(
 			'\x1b[31m%s\x1b[0m',
