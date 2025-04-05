a434a39a0f7d25368947db47f5abb066ab0bf035
KIO2gamer <77529753+KIO2gamer@users.noreply.github.com>
2025-04-05 19:14:20 +0530

    Added ban command

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -3,8 +3,11 @@ const { SlashCommandBuilder } = require('discord.js')
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ban')
-        .setDescription('ban'),
+        .setDescription('ban')
+        .addUserOption(option => option.setName('user').setDescription('Select the user to ban')),
     async execute(interaction) {
-        await interaction.reply('interaction provoked')
+        const user = interaction.options.getUser('user')
+        interaction.guild.members.ban(user)
+        await interaction.reply(`${user.tag} is banned!!!`)
     }
 }
